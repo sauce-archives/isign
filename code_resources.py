21a6aa3fb967f77d854642e46362e32142abf270
Neil Kandalgaonkar <neilk@brevity.org>
2015-05-11 15:51:54 -0700

    parsing, using, emitting rules

--- code_resources.py
+++ code_resources.py
@@ -5,16 +5,160 @@ import hashlib
 from optparse import OptionParser
 import os
 import plistlib
-# import re
+from plistlib import PlistWriter
+import re
 # import subprocess
 
 OUTPUT_DIRECTORY = '_CodeSignature'
 OUTPUT_FILENAME = 'CodeResources'
 TEMPLATE_FILENAME = 'code_resources_template.xml'
-DIGEST_ALGORITHM = "sha1"
+# DIGEST_ALGORITHM = "sha1"
 HASH_BLOCKSIZE = 65536
 
 # OPENSSL = os.getenv('OPENSSL', distutils.spawn.find_executable('openssl'))
+
+
+# have to monkey patch Plist, in order to make the values
+# look the same - no .0 for floats
+# Apple's plist utils work like this:
+#   1234.5 --->  <real>1234.5</real>
+#   1234.0 --->  <real>1234</real>
+def writeValue(self, value):
+    if isinstance(value, float):
+        rep = repr(value)
+        if value.is_integer():
+            rep = repr(int(value))
+        self.simpleElement("real", rep)
+    else:
+        self.oldWriteValue(value)
+
+PlistWriter.oldWriteValue = PlistWriter.writeValue
+PlistWriter.writeValue = writeValue
+
+
+# Simple reimplementation of ResourceBuilder, in the Apple Open Source
+# file bundlediskrep.cpp
+class PathRule(object):
+    OPTIONAL = 0x01
+    OMITTED = 0x02
+    NESTED = 0x04
+    EXCLUSION = 0x10  # unused?
+    TOP = 0x20        # unused?
+
+    def __init__(self, pattern='', properties=None):
+        print "--- init --"
+        # on Mac OS the FS is case-insensitive; simulate that here
+        self.pattern = re.compile(pattern, re.IGNORECASE)
+        self.flags = 0
+        self.weight = 0
+        print "pattern: {0}, properties: {1}".format(pattern, properties)
+        if properties is not None:
+            print "props not none"
+            if type(properties) == 'bool':
+                print "props is bool"
+                if properties is False:
+                    self.flags |= PathRule.OMITTED
+                # if it was true, this file is required;
+                # do nothing
+            elif isinstance(properties, dict):
+                print "props is dict"
+                for key, value in properties.iteritems():
+                    print "prop is {0},{1}".format(key, value)
+                    if key == 'optional' and value is True:
+                        self.flags |= PathRule.OPTIONAL
+                    elif key == 'omit' and value is True:
+                        self.flags |= PathRule.OMITTED
+                    elif key == 'nested' and value is True:
+                        self.flags |= PathRule.NESTED
+                    elif key == 'weight':
+                        self.weight = float(value)
+        print type(properties)
+        print str(self)
+
+    def is_optional(self):
+        return self.flags & PathRule.OPTIONAL != 0
+
+    def is_omitted(self):
+        return self.flags & PathRule.OMITTED != 0
+
+    def is_nested(self):
+        return self.flags & PathRule.NESTED != 0
+
+    def is_exclusion(self):
+        return self.flags & PathRule.EXCLUSION != 0
+
+    def is_top(self):
+        return self.flags & PathRule.TOP != 0
+
+    def matches(self, path):
+        return re.match(self.pattern, path)
+
+    def __str__(self):
+        return 'PathRule:' + str(self.flags) + ':' + str(self.weight)
+
+
+class ResourceBuilder(object):
+    NULL_PATH_RULE = PathRule()
+
+    def __init__(self, rules_data):
+        self.rules = []
+        for pattern, properties in rules_data.iteritems():
+            self.rules.append(PathRule(pattern, properties))
+
+    def find_rule(self, path):
+        best_rule = ResourceBuilder.NULL_PATH_RULE
+        for rule in self.rules:
+            # print 'trying rule ' + str(rule) + ' against ' + path
+            if rule.matches(path):
+                if rule.flags and rule.is_exclusion():
+                    best_rule = rule
+                    break
+                elif rule.weight > best_rule.weight:
+                    best_rule = rule
+        return best_rule
+
+    def get_rule_and_paths(self, root, path):
+        path = os.path.join(root, path)
+        relative_path = os.path.relpath(path, source_dir)
+        rule = self.find_rule(relative_path)
+        return (rule, path, relative_path)
+
+    def scan(self, source_dir):
+        """
+        Walk entire directory, compile mapping
+        path relative to source_dir -> digest and other data
+        """
+        file_entries = {}
+        for root, dirs, filenames in os.walk(source_dir):
+
+            for filename in filenames:
+                rule, path, relative_path = self.get_rule_and_paths(root,
+                                                                    filename)
+
+                print rule
+                if rule.is_omitted() or rule.is_exclusion():
+                    continue
+
+                # the Data element in plists is base64-encoded
+                val = {'hash': plistlib.Data(get_hash_binary(path))}
+
+                if rule.is_optional():
+                    val['optional'] = True
+
+                if len(val) == 1 and 'hash' in val:
+                    file_entries[relative_path] = val['hash']
+                else:
+                    file_entries[relative_path] = val
+
+            for dirname in dirs:
+                rule, path, relative_path = self.get_rule_and_paths(root,
+                                                                    dirname)
+
+                if rule.is_nested() and '.' not in path:
+                    dirs.remove(dirname)
+                    continue
+
+        return file_entries
 
 
 def get_template():
@@ -63,33 +207,6 @@ def get_hash_binary(path):
     return binascii.a2b_hex(get_hash_hex(path))
 
 
-def is_optional(path, rules):
-    return 'Assets.car' not in path
-
-
-def get_file_entries(source_dir, rules):
-    """
-    Walk entire directory, compile mapping
-    path relative to source_dir -> digest and other data
-    """
-    file_entries = {}
-    for root, dirs, filenames in os.walk(source_dir):
-        for filename in filenames:
-            path = os.path.join(root, filename)
-            # the Data element in plists is base64-encoded
-            data = plistlib.Data(get_hash_binary(path))
-            relpath_to_source = os.path.relpath(path, source_dir)
-            if is_optional(path, rules):
-                val = {
-                    'data': data,
-                    'optional': True
-                }
-            else:
-                val = data
-            file_entries[relpath_to_source] = val
-    return file_entries
-
-
 def write_plist(target_dir, plist):
     """ Write the CodeResources file """
     output_dir = os.path.join(target_dir, OUTPUT_DIRECTORY)
@@ -112,7 +229,8 @@ def main(source_dir, target_dir):
     # deciding which files should be part of the seal
     rules = template['rules2']
     plist = copy.deepcopy(template)
-    plist['files'] = get_file_entries(source_dir, rules)
+    resource_builder = ResourceBuilder(rules)
+    plist['files'] = resource_builder.scan(source_dir)
     write_plist(target_dir, plist)
 
 
