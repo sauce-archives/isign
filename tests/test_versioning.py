1e7a9044fc814465fa38e4c914d2bba0a3dc3117
jeremy avnet <brainsik-github@theory.org>
2015-06-10 00:24:36 -0700

    Add versioning tests

---
+++ tests/test_versioning.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import unittest
+
+import sauce_stats
+
+
+class VersioningTestCase(unittest.TestCase):
+
+    def assert_proper_attribute(self, attribute):
+        try:
+            assert getattr(sauce_stats, attribute), (
+                "{} improperly set".format(attribute))
+        except AttributeError:
+            assert False, "missing {}".format(attribute)
+
+    def test_version_attribute(self):
+        self.assert_proper_attribute("__version__")
+
+        # test major, minor, and patch are numbers
+        version_split = sauce_stats.__version__.split(".")[:3]
+        assert version_split, "__version__ is not set"
+        for n in version_split:
+            try:
+                int(n)
+            except ValueError:
+                assert False, "'{}' is not an integer".format(n)
+
+    def test_commit_attribute(self):
+        self.assert_proper_attribute("__commit__")
+
+    def test_build_attribute(self):
+        self.assert_proper_attribute("__build__")
+
+
+if __name__ == '__main__':
+    unittest.main()
