942bf503cb39c77519d5079e31bb2e3efe5c856d
Neil Kandalgaonkar <neilk@brevity.org>
2015-06-26 14:06:42 -0700

    use temp files for tests

--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -11,6 +11,7 @@ from os.path import exists
 from isign import isign
 import shutil
 import unittest
+import tempfile
 
 
 class TestPublicInterface(unittest.TestCase):
@@ -19,6 +20,11 @@ class TestPublicInterface(unittest.TestCase):
         "certificate": CERTIFICATE,
         "provisioning_profile": PROVISIONING_PROFILE
     }
+
+    def _get_temp_file(self):
+        (fd, path) = tempfile.mkstemp()
+        os.close(fd)
+        return path
 
     def _remove(self, path):
         if exists(path):
@@ -32,27 +38,26 @@ class TestPublicInterface(unittest.TestCase):
         args.update(self.credentials)
         return isign.resign(filename, **args)
 
-    def _test_signable(self, filename):
+    def _test_signable(self, filename, output_path):
         with isign.new_from_archive(filename) as app:
-            output_path = os.tmpnam()
             resigned_path = self._resign(app, output_path=output_path)
             assert exists(resigned_path)
+            assert os.path.getsize(resigned_path) > 0
             self._remove(resigned_path)
 
-    def _test_unsignable(self, filename):
+    def _test_unsignable(self, filename, output_path):
         with self.assertRaises(isign.NotSignable):
             with isign.new_from_archive(filename) as app:
-                output_path = os.tmpnam()
                 self._resign(app, output_path=output_path)
 
     def test_app(self):
-        self._test_signable(TEST_APP)
+        self._test_signable(TEST_APP, tempfile.mkdtemp())
 
     def test_app_ipa(self):
-        self._test_signable(TEST_IPA)
+        self._test_signable(TEST_IPA, self._get_temp_file())
 
     def test_non_app_txt(self):
-        self._test_unsignable(TEST_NONAPP_TXT)
+        self._test_unsignable(TEST_NONAPP_TXT, self._get_temp_file())
 
     def test_non_app_ipa(self):
-        self._test_unsignable(TEST_NONAPP_IPA)
+        self._test_unsignable(TEST_NONAPP_IPA, self._get_temp_file())
