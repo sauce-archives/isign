b797f120712f9072018247d2f1efb44217f86e02
Neil Kandalgaonkar <neilk@brevity.org>
2015-06-26 14:07:22 -0700

    quieter logs

--- isign/log_to_stderr.py
+++ isign/log_to_stderr.py
@@ -6,4 +6,4 @@ def log_to_stderr(log):
     format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(stream=sys.stderr,
                         format=format_str,
-                        level=logging.DEBUG)
+                        level=logging.INFO)
