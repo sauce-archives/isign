94b79ab30827ba895a680da7cadb9e7174479cea
Neil Kandalgaonkar <neilk@brevity.org>
2015-06-25 20:24:53 -0700

    changed interface to be less magical
    Previously the public interface used resign() to mean something
    like "resign if it can be resigned". Others at Sauce objected
    to this <https://github.com/saucelabs/sauce/pull/1190/>
    
    It's not really feasible to do a separate test for resign-ability
    apart from creating the app object, but we can throw exceptions

---
+++ tests/common_isign_test.py
@@ -0,0 +1,20 @@
+from os.path import abspath
+from os.path import dirname
+from os.path import exists
+from os.path import join
+
+TEST_DIR = dirname(__file__)
+TEST_APP = join(TEST_DIR, 'SimpleSaucyApp.app')
+TEST_APPZIP = TEST_APP + '.zip'
+TEST_APPTGZ = join(TEST_DIR, 'SimpleSaucyApp.tgz')
+TEST_IPA = join(TEST_DIR, 'SimpleSaucyApp.ipa')
+TEST_NONAPP_TXT = join(TEST_DIR, 'NotAnApp.txt')
+TEST_NONAPP_IPA = join(TEST_DIR, 'NotAnApp.ipa')
+REPO_ROOT = dirname(dirname(abspath(__file__)))
+ISIGN_BIN = join(REPO_ROOT, 'isign', 'isign.py')
+KEY = join(TEST_DIR, 'credentials', 'test.key.pem')
+CERTIFICATE = join(TEST_DIR, 'credentials', 'test.cert.pem')
+PROVISIONING_PROFILE = join(TEST_DIR, 'credentials', 'test.mobileprovision')
+ERROR_KEY = '_errors'
+# Sauce Labs apple organizational unit
+OU = 'JWKXD469L2'
