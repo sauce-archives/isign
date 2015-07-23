c2edfaad415b15b1f277184b66ff89c811959695
Neil Kandalgaonkar <neilk@brevity.org>
2015-07-23 16:22:51 -0700

    sort out logging and error reporting, verbosity

--- isign/log_to_stderr.py
+++ isign/log_to_stderr.py
@@ -1,9 +1,11 @@
-import sys
 import logging
 
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def log_to_stderr(log):
-    format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(stream=sys.stderr,
-                        format=format_str,
-                        level=logging.INFO)
+
+def log_to_stderr(level=logging.INFO):
+    root = logging.getLogger()
+    root.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setFormatter(FORMATTER)
+    root.addHandler(handler)
