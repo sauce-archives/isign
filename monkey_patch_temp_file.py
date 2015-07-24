068ce24f02e378e23f2c93d91c4adf5531915b45
Neil Kandalgaonkar <neilk@brevity.org>
2015-07-24 12:52:09 -0700

    test for temp files not cleaned up

---
+++ monkey_patch_temp_file.py
@@ -0,0 +1,75 @@
+""""
+isign creates big temporary files, using the standard tempfile library.
+If they are not cleaned up, they can fill up the disk. This has
+already happened in production. :(
+
+directory, so it's easy to test that we aren't leaving any temp files behind.
+"""
+import os
+import shutil
+import tempfile
+
+
+class MonkeyPatchTempFile(object):
+    TEMP_DIR = None
+
+    @classmethod
+    def mkdtemp(cls, *args, **kwargs):
+        """ ensure temp directories are subdirs of TEMP_DIR """
+        kwargs['dir'] = MonkeyPatchTempFile.TEMP_DIR
+        return tempfile._original_mkdtemp(*args, **kwargs)
+
+    @classmethod
+    def mkstemp(cls, *args, **kwargs):
+        """ ensure temp files are within TEMP_DIR """
+        kwargs['dir'] = MonkeyPatchTempFile.TEMP_DIR
+        return tempfile._original_mkstemp(*args, **kwargs)
+
+    @classmethod
+    def NamedTemporaryFile(cls, *args, **kwargs):
+        """ ensure named temp files are within TEMP_DIR """
+        kwargs['dir'] = MonkeyPatchTempFile.TEMP_DIR
+        return tempfile._original_NamedTemporaryFile(*args, **kwargs)
+
+    @classmethod
+    def patch(cls):
+        """ swap a few methods in tempfile with our versions that limit them
+            to a particular directory """
+
+        if hasattr(tempfile, '_is_patched') and tempfile._is_patched:
+            raise Exception("need tempfile to be in unpatched state!")
+
+        cls.TEMP_DIR = tempfile.mkdtemp(prefix='isign-test-run-')
+
+        tempfile._original_mkdtemp = tempfile.mkdtemp
+        tempfile.mkdtemp = MonkeyPatchTempFile.mkdtemp
+
+        tempfile._original_mkstemp = tempfile.mkstemp
+        tempfile.mkstemp = MonkeyPatchTempFile.mkstemp
+
+        tempfile._original_NamedTemporaryFile = tempfile.NamedTemporaryFile
+        tempfile.NamedTemporaryFile = MonkeyPatchTempFile.NamedTemporaryFile
+
+        tempfile._is_patched = True
+
+    @classmethod
+    def restore(cls):
+        """ restore a few methods in tempfile. opposite of _tempfile_patch """
+        tempfile.mkdtemp = tempfile._original_mkdtemp
+        tempfile.mkstemp = tempfile._original_mkstemp
+        tempfile.NamedTemporaryFile = tempfile._original_NamedTemporaryFile
+
+        tempfile._is_patched = False
+
+        shutil.rmtree(cls.TEMP_DIR)
+
+        cls.TEMP_DIR = None
+
+    @classmethod
+    def has_no_temp_files(cls):
+        """ check if this test has created any temp files which
+            aren't cleaned up """
+        if cls.TEMP_DIR is None:
+            raise Exception("temp dir is None. Maybe call patch() first?")
+
+        return len(os.listdir(cls.TEMP_DIR)) == 0
