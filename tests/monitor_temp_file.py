e333928222b6a77916f63b63ffce45b22fc0288c
Neil Kandalgaonkar <neilk@brevity.org>
2015-07-24 13:00:20 -0700

    move MonkeyPatchTempFile to monitor_temp_file.py

--- tests/monitor_temp_file.py
+++ tests/monitor_temp_file.py
@@ -3,6 +3,7 @@ isign creates big temporary files, using the standard tempfile library.
 If they are not cleaned up, they can fill up the disk. This has
 already happened in production. :(
 
+This library monkey-patches tempfile to use our own temporary
 directory, so it's easy to test that we aren't leaving any temp files behind.
 """
 import os
@@ -10,29 +11,29 @@ import shutil
 import tempfile
 
 
-class MonkeyPatchTempFile(object):
+class MonitorTempFile(object):
     TEMP_DIR = None
 
     @classmethod
     def mkdtemp(cls, *args, **kwargs):
         """ ensure temp directories are subdirs of TEMP_DIR """
-        kwargs['dir'] = MonkeyPatchTempFile.TEMP_DIR
+        kwargs['dir'] = MonitorTempFile.TEMP_DIR
         return tempfile._original_mkdtemp(*args, **kwargs)
 
     @classmethod
     def mkstemp(cls, *args, **kwargs):
         """ ensure temp files are within TEMP_DIR """
-        kwargs['dir'] = MonkeyPatchTempFile.TEMP_DIR
+        kwargs['dir'] = MonitorTempFile.TEMP_DIR
         return tempfile._original_mkstemp(*args, **kwargs)
 
     @classmethod
     def NamedTemporaryFile(cls, *args, **kwargs):
         """ ensure named temp files are within TEMP_DIR """
-        kwargs['dir'] = MonkeyPatchTempFile.TEMP_DIR
+        kwargs['dir'] = MonitorTempFile.TEMP_DIR
         return tempfile._original_NamedTemporaryFile(*args, **kwargs)
 
     @classmethod
-    def patch(cls):
+    def start(cls):
         """ swap a few methods in tempfile with our versions that limit them
             to a particular directory """
 
@@ -42,18 +43,18 @@ class MonkeyPatchTempFile(object):
         cls.TEMP_DIR = tempfile.mkdtemp(prefix='isign-test-run-')
 
         tempfile._original_mkdtemp = tempfile.mkdtemp
-        tempfile.mkdtemp = MonkeyPatchTempFile.mkdtemp
+        tempfile.mkdtemp = MonitorTempFile.mkdtemp
 
         tempfile._original_mkstemp = tempfile.mkstemp
-        tempfile.mkstemp = MonkeyPatchTempFile.mkstemp
+        tempfile.mkstemp = MonitorTempFile.mkstemp
 
         tempfile._original_NamedTemporaryFile = tempfile.NamedTemporaryFile
-        tempfile.NamedTemporaryFile = MonkeyPatchTempFile.NamedTemporaryFile
+        tempfile.NamedTemporaryFile = MonitorTempFile.NamedTemporaryFile
 
         tempfile._is_patched = True
 
     @classmethod
-    def restore(cls):
+    def stop(cls):
         """ restore a few methods in tempfile. opposite of _tempfile_patch """
         tempfile.mkdtemp = tempfile._original_mkdtemp
         tempfile.mkstemp = tempfile._original_mkstemp
