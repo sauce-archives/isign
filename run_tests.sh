d8db81db91872d6a95f65efa86abeed8367b3891
jeremy avnet <brainsik@saucelabs.com>
2015-07-30 11:21:53 -0700

    Test for the apps required for testing
    This has bitten me multiple times with a bizarre error when you don't
    have unzip. Test for it upfront!
    
    Review: @neilk

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# look for required apps
+for app in unzip zip; do
+    if ! which $app >/dev/null; then
+        echo "Missing application: $app"
+        exit 1
+    fi
+done
+
 set -e
 ./version.sh json | python -mjson.tool
 find . -name '*.pyc' -delete
