e258bd94d62b98b1fef18bfbc324fc44dd5a273c
Neil Kandalgaonkar <neilk@brevity.org>
2015-07-31 17:43:20 -0700

    MASTER-1801, MASTER-1802 refactor, bump version
    Refactor!
    
    Formerly, we required the caller to use the with/as
    construct to clean up temp files. Now it does this
    internally with a simple try/catch.
    
    The external interface is just isign.resign() now. It
    may raise isign.NotSignable if you ask it to sign something
    that isn't an iOS native app.
    
    Doesn't create any temp files until it actually starts working
    on them. Examines .zip and .ipa files with `zipfile` to see if
    they are iOS native apps, without unpacking them.
    
    A .zip or .ipa now is considered to *have* an app inside it, rather
    than being a subtype of an app. This separates directory structure
    and archive format from the business of re-signing.

--- isign/isign.py
+++ isign/isign.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import app as application
+import app
 import argparse
 import logging
 from log_to_stderr import log_to_stderr
@@ -8,9 +8,6 @@ from log_to_stderr import log_to_stderr
 import os
 import os.path
 from os.path import dirname, join, realpath
-from signer import Signer
-import sys
-
 
 # this comes with the repo
 PACKAGE_ROOT = dirname(realpath(__file__))
@@ -28,22 +25,41 @@ log = logging.getLogger(__name__)
 
 
 class NotSignable(Exception):
-    """ all purpose exception """
+    """ This is just so we don't expose isign.app.NotSignable """
     pass
 
+
+def resign(input_path,
+           certificate=CERTIFICATE_PATH,
+           key=KEY_PATH,
+           apple_cert=APPLE_CERT_PATH,
+           provisioning_profile=PROVISIONING_PROFILE_PATH,
+           output_path=join(os.getcwd(), "out")):
+    """ simply for convenience, and to omit default args """
+    try:
+        return app.resign(input_path,
+                          certificate,
+                          key,
+                          apple_cert,
+                          provisioning_profile,
+                          output_path)
+    except app.NotSignable as e:
+        # re-raise the exception without exposing internal
+        # details of how it happened
+        raise NotSignable(e)
+
+
+# The rest is all about parsing command line args
+# could be moved to a different 'binary'
 
 def absolute_path_argument(path):
     return os.path.abspath(os.path.expanduser(path))
 
 
-def exists_absolute_path_argument(path):
-    path = absolute_path_argument(path)
-    if not os.path.exists(path):
-        raise argparse.ArgumentTypeError("%s does not exist!" % path)
-    return path
-
-
 def parse_args():
+    # note that for arguments which eventually get fed into
+    # isign.resign, we deliberately don't set defaults. The kwarg
+    # defaults in isign.resign will be used
     parser = argparse.ArgumentParser(
         description='Resign an iOS application with a new identity '
                     'and provisioning profile.')
@@ -52,7 +68,7 @@ def parse_args():
         dest='provisioning_profile',
         required=False,
         metavar='<your.mobileprovision path>',
-        type=exists_absolute_path_argument,
+        type=absolute_path_argument,
         help='Path to provisioning profile'
     )
     parser.add_argument(
@@ -60,7 +76,7 @@ def parse_args():
         dest='apple_cert',
         required=False,
         metavar='<apple cert>',
-        type=exists_absolute_path_argument,
+        type=absolute_path_argument,
         help='Path to Apple certificate in .pem form'
     )
     parser.add_argument(
@@ -68,7 +84,7 @@ def parse_args():
         dest='key',
         required=False,
         metavar='<key path>',
-        type=exists_absolute_path_argument,
+        type=absolute_path_argument,
         help='Path to your organization\'s key in .p12 format'
     )
     parser.add_argument(
@@ -76,7 +92,7 @@ def parse_args():
         dest='certificate',
         required=False,
         metavar='<certificate path>',
-        type=exists_absolute_path_argument,
+        type=absolute_path_argument,
         help='Path to your organization\'s certificate in .pem form'
     )
     parser.add_argument(
@@ -85,14 +101,13 @@ def parse_args():
         required=False,
         metavar='<output path>',
         type=absolute_path_argument,
-        default=None,
         help='Path to output file or directory'
     )
     parser.add_argument(
         'app_paths',
         nargs=1,
         metavar='<app path>',
-        type=exists_absolute_path_argument,
+        type=absolute_path_argument,
         help='Path to application to re-sign, typically a '
              'directory ending in .app or file ending in .ipa.'
     )
@@ -108,66 +123,27 @@ def parse_args():
     return parser.parse_args()
 
 
-def new_from_archive(path):
-    """ facade mostly so it is easy to import
-        and to re-raise under one Exception class """
-    try:
-        app = application.new_from_archive(path)
-    except application.NotSignable as e:
-        log.error(e)
-        raise NotSignable(e)
-    return app
-
-
-def resign(app,
-           certificate=CERTIFICATE_PATH,
-           key=KEY_PATH,
-           apple_cert=APPLE_CERT_PATH,
-           provisioning_profile=PROVISIONING_PROFILE_PATH,
-           output_path=os.path.join(os.getcwd(), 'out')):
-    """ Given app object, returns path of newly resigned app """
-
-    signer = Signer(signer_cert_file=certificate,
-                    signer_key_file=key,
-                    apple_cert_file=apple_cert)
-
-    app.provision(provisioning_profile)
-    app.create_entitlements(signer.team_id)
-    app.sign(signer)
-    app.package(output_path)
-    log.info("Created resigned app at <%s>", output_path)
-
-    return output_path
-
-
 if __name__ == '__main__':
     args = parse_args()
-    args_dict = vars(args)
-    args_dict['input_path'] = args.app_paths[0]
-    del args_dict['app_paths']
 
-    # make sure defaults are triggered properly in
-    # the signature for resign() -- rather than a value of None,
-    # they should be absent
-    for key in args_dict.keys():
-        if key in args_dict and args_dict[key] is None:
-            del args_dict[key]
-
-    if args_dict['verbose']:
+    if args.verbose:
         level = logging.DEBUG
     else:
         level = logging.INFO
-    del args_dict['verbose']
-
     log_to_stderr(level)
 
-    input_path = args_dict['input_path']
-    del args_dict['input_path']
+    # Filter out args that don't matter to the
+    # resign call, like verbosity or help
+    resign_args = ['certificate',
+                   'key',
+                   'apple_cert',
+                   'provisioning_profile',
+                   'output_path']
+    kwargs = {}
+    # we want the unused command line args to be
+    # missing in kwargs, so the defaults are used
+    for k, v in vars(args).iteritems():
+        if k in resign_args and v is not None:
+            kwargs[k] = v
 
-    try:
-        with application.new_from_archive(input_path) as app:
-            resign(app, **args_dict)
-    except NotSignable as e:
-        msg = "Not signable: <{0}>: {1}\n".format(input_path, e)
-        log.error(msg)
-        sys.exit(1)
+    resign(args.app_paths[0], **kwargs)
