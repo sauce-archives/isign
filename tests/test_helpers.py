c7bb864389326c46a5c69f13a1380cfa399a948d
Neil Kandalgaonkar <neilk@brevity.org>
2015-08-24 13:49:22 -0700

    MASTER-502 test for helpers better
    If a helper app isn't present, log an informative message and
    keep checking for it when new requests come in.
    
    So, there's now a possibility you can install the dependency and
    it will Just Work. This is better than requiring a new deploy to
    restart the service properly.

---
+++ tests/test_helpers.py
@@ -0,0 +1,36 @@
+import isign.app
+from isign.app import AppZip
+from isign.app import NotSignable
+from isign_base_test import IsignBaseTest
+from distutils import spawn
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class MissingHelpersApp(AppZip):
+    """ An App whose helpers are not present """
+    helpers = ['a_file_that_should_never_be_present']
+
+
+def dummy_find_executable(name):
+    return '/dummy/path/to/name'
+
+
+class TestHelpers(IsignBaseTest):
+    def test_helpers_is_present(self):
+        """ test that missing helpers raises exception """
+        with self.assertRaises(NotSignable):
+            MissingHelpersApp(self.TEST_APPZIP)
+
+    def test_helpers_become_present(self):
+        """ test that we can install helpers without restart """
+        with self.assertRaises(NotSignable):
+            MissingHelpersApp(self.TEST_APPZIP)
+        spawn._original_find_executable = spawn.find_executable
+        spawn.find_executable = dummy_find_executable
+        MissingHelpersApp(self.TEST_APPZIP)
+        if hasattr(spawn, '_original_find_executable'):
+            spawn.find_executable = spawn._original_find_executable
+        isign.app.helper_paths = {}
