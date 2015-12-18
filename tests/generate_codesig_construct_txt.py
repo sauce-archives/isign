671d311f32bea7590c9548ab44a47d9817c52c6a
Neil Kandalgaonkar <neilk@brevity.org>
2015-12-18 12:40:45 -0800

    swift 2 tests - now open source

---
+++ tests/generate_codesig_construct_txt.py
@@ -0,0 +1,28 @@
+# generate a string representation of a signature parse for an app - do this to generate
+# test files such as Test.app.codesig.construct.txt
+
+from isign_base_test import IsignBaseTest
+import isign.app
+from isign.signable import Executable
+
+import logging
+
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+def log_to_stderr(level=logging.INFO):
+    root = logging.getLogger()
+    root.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setFormatter(FORMATTER)
+    root.addHandler(handler)
+
+
+log_to_stderr(logging.DEBUG)
+log = logging.getLogger(__name__)
+log.debug("generating a signature parse for an app")
+app = isign.app.App(IsignBaseTest.TEST_APP)
+executable = Executable(app.get_executable_path())
+arch = executable.arches[0]
+codesig_str = str(arch['cmds']['LC_CODE_SIGNATURE'])
+print codesig_str
